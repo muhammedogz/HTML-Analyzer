4ea809cb730c9ce3b5850daab0d92934b981145e
Muhammed Oguz <muhammed0748@outlook.com>
2022-12-04 03:17:08 +0300

    Change cors

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,23 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: MyAllowSpecificOrigins,
+                    policy =>
+                    {
+                      policy.WithOrigins("http://localhost:5173/HTML-Analyzer/",
+                                            "https://muhammedogz.github.io/HTML-Analyzer/",
+                                            // localhost
+                                            "http://localhost:5173",
+                                            "http://localhost:5173/",
+                                            "http://localhost:5173/HTML-Analyzer",
+                                            // github
+                                            "https://muhammedogz.github.io"
+                                            );
+                    });
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -10,6 +28,7 @@ var app = builder.Build();
 // if (app.Environment.IsDevelopment())
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 app.MapControllers();
