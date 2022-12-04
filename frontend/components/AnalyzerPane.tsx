28b7323f240b77da948f96e928de4d9efbcdd646
Muhammed Oguz <muhammed0748@outlook.com>
2022-12-05 01:28:08 +0300

    Add prettier functionaliy

--- frontend/components/AnalyzerPane.tsx
+++ frontend/components/AnalyzerPane.tsx
@@ -1,5 +1,4 @@
-import { Typography } from "@mui/material";
-import { Stack } from "@mui/system";
+import { Stack } from '@mui/system';
 
 interface IAnalyzerPaneProps {
   children: React.ReactNode;
@@ -9,10 +8,10 @@ const AnalyzerPane = ({ children }: IAnalyzerPaneProps) => {
   return (
     <Stack
       sx={{
-        background: "white",
-        borderRadius: "20px",
-        boxShadow: "0 0 15px 1px rgba(0, 0, 0, 0.4)",
-        padding: "20px 30px",
+        background: 'white',
+        borderRadius: '20px',
+        boxShadow: '0 0 15px 1px rgba(0, 0, 0, 0.4)',
+        padding: '20px 30px',
       }}
     >
       {children}
